9a0308c830546fd3592217bc465b9da3f9e237b6
Garrafinha <andrerampanelli1@gmail.com>
2019-12-24 13:12:24 -0300

    Ajustes API

--- api/product/create.php
+++ api/product/create.php
@@ -2,7 +2,7 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: *");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
@@ -30,15 +30,12 @@ if (
 
     if ($product->create()) {
         http_response_code(201);
-
         echo json_encode(array("message" => "Product was created."));
     } else {
         http_response_code(503);
-
         echo json_encode(array("message" => "Unable to create product."));
     }
 } else {
     http_response_code(400);
-
     echo json_encode(array("message" => "Unable to create product. Data is incomplete."));
 }
--- api/product/update.php
+++ api/product/update.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: *");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
@@ -23,13 +23,9 @@ $product->description = $data->description;
 $product->category_id = $data->category_id;
 
 if ($product->update()) {
-
-    http_response_code(200);
-
     echo json_encode(array("message" => "Product was updated."));
+    http_response_code(200);
 } else {
-
-    http_response_code(503);
-
     echo json_encode(array("message" => "Unable to update product."));
+    http_response_code(503);
 }
